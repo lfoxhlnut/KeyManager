2c713cb1c2d3af71b7973170bafc8bcd2cae4964
想全世界停电 <x-helen@foxmail.com>
2024-03-22 17:08:24 +0800

    conversion between Data and String

--- script/Content.gd
+++ script/Content.gd
@@ -6,5 +6,9 @@ extends Control
 @onready var v_box: VBoxContainer = $VBoxContainer
 
 
+func _ready() -> void:
+	var t := Data.new("4", ["f", "中1文"])
+	print(t)
+
 func initialize() -> void:
 	pass
--- script/data.gd
+++ script/data.gd
@@ -1,9 +1,11 @@
 class_name Data
 extends RefCounted
 
+const delimiter = "`"
+
 var title: String
 #var info_num: int
-var info: Array[String] = []:
+var info: PackedStringArray = []:
 	set(v):
 		info = v.duplicate()
 
@@ -14,4 +16,12 @@ func _init(_title: String = "", _info: Array[String] = []) -> void:
 
 
 func _to_string() -> String:
-	return "%s`%s" % [title, info]
+	return "%s`%s" % [title, "`".join(info)]
+
+
+func from_string(s: String) -> void:
+	var pieces := s.split(delimiter)
+	title = pieces[0]
+	info.clear()
+	for i: int in range(2, pieces.size()):
+		info.append(pieces[i])
