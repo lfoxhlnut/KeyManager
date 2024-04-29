d52b1b298f09af53e9f40bfa55712a615fdb12ea
想全世界停电 <x-helen@foxmail.com>
2024-04-29 18:41:32 +0800

    Signals connect with Android menu buttons.

---
+++ script/android/sub_menu_btn.gd
@@ -0,0 +1,34 @@
+@tool
+extends Control
+
+@onready var save_operation: HBoxContainer = $SaveOperation
+@onready var item_operation: HBoxContainer = $ItemOperation
+@onready var class_operation: HBoxContainer = $ClassOperation
+
+func _ready() -> void:
+	for i: HBoxContainer in get_children():
+		i.position = Vector2.ZERO
+		i.size = size
+
+
+func hide_children() -> void:
+	# Callable expected by all() should return a bool value,
+	# otherwise the callable will be called only once.
+	get_children().all(func(i: Control):
+		i.hide()
+		return true
+	)
+
+func _on_save_op_pressed() -> void:
+	hide_children()
+	save_operation.show()
+
+
+func _on_item_op_pressed() -> void:
+	hide_children()
+	item_operation.show()
+
+
+func _on_class_op_pressed() -> void:
+	hide_children()
+	class_operation.show()
